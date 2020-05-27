3a33bbfd8fb622ed7b3e20479b67e055de8068d3
Arhan13 <60483503+Arhan13@users.noreply.github.com>
2020-05-27 22:50:01 +0530

    updating our app to redirect to home if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,8 @@
+import { UserActionTypes} from './user.types';
+
 //Functions that return action objects
 //object should be in correct format which the action is expected to be
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 });
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,3 +1,5 @@
+import { UserActionTypes } from'./user.types';
+
 //Recieves an current user object and action containig a string value
 const INITIAL_STATE = {
     currentUser:null
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => { //state here is basically the current state we are giving it a fallback
     switch(action.type) { //action.type is basically the string
-        case 'SET_CURRENT_USER' :
+        case UserActionTypes.SET_CURRENT_USER :
             return {
                 ...state,
                 currentUser: action.payload
